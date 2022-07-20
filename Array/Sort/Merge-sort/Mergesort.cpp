d7c81e3d8ca185fe1d1fa69cfb1ed0763f93d1c0
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-20 23:50:00 +0530

    quick sort

--- Array/Sort/Merge-sort/Mergesort.cpp
+++ Array/Sort/Merge-sort/Mergesort.cpp
@@ -1,53 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void Merge(int *arr,int lb, int ub){
-	int mid=(lb+ub)/2;
-	int len1=mid-lb+1;
-	int len2=ub-mid;
-	int a[len1],b[len2],i=0,j=0,c=lb;
+void Merge(int *arr, int lb, int ub)
+{
+	int mid = (lb + ub) / 2;
+	int len1 = mid - lb + 1;
+	int len2 = ub - mid;
+	int a[len1], b[len2], i = 0, j = 0, c = lb;
 
 	for (int i = 0; i < len1; ++i)
 	{
-		a[i]=arr[c++];
+		a[i] = arr[c++];
 	}
-	c=mid+1;
+	c = mid + 1;
 	for (int i = 0; i < len2; ++i)
 	{
-		b[i]=arr[c++];
+		b[i] = arr[c++];
 	}
-	c=lb;
-	while(i<len1 && j<len2){
-		if(a[i]<=b[j]){
-			arr[c++]=a[i++];
+	c = lb;
+	while (i < len1 && j < len2)
+	{
+		if (a[i] <= b[j])
+		{
+			arr[c++] = a[i++];
 		}
-		else{
-			arr[c++]=b[j++];
+		else
+		{
+			arr[c++] = b[j++];
 		}
 	}
 
-	while(i<len1){
-		arr[c++]=a[i++];
+	while (i < len1)
+	{
+		arr[c++] = a[i++];
 	}
-	while(j<len2){
-		arr[c++]=b[j++];
+	while (j < len2)
+	{
+		arr[c++] = b[j++];
 	}
 }
 
-void MergeSort(int *arr,int lb, int ub){
-	if(lb>=ub){return;}
-	int mid=(lb+ub)/2;
-	MergeSort(arr,lb,mid);
-	MergeSort(arr,mid+1,ub);
-	Merge(arr,lb,ub);
+void MergeSort(int *arr, int lb, int ub)
+{
+	if (lb >= ub)
+	{
+		return;
+	}
+	int mid = (lb + ub) / 2;
+	MergeSort(arr, lb, mid);
+	MergeSort(arr, mid + 1, ub);
+	Merge(arr, lb, ub);
 }
 
 int main()
 {
-	int arr[8]={1,2,6,3,4,55,6,7};
-	MergeSort(arr,0,sizeof(arr)/sizeof(arr[0])-1);
-	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); ++i)
+	int arr[8] = {1, 2, 6, 3, 4, 55, 6, 7};
+	MergeSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 	{
-		cout<<arr[i]<<" ";
+		cout << arr[i] << " ";
 	}
 	return 0;
 }
--- Array/Sort/quicksort/main.c
+++ Array/Sort/quicksort/main.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-int Position(int *a,int lb,int ub)
+int Position(int *a, int lb, int ub)
 {
-int pivot=a[lb],temp, start=lb, end=ub;
-//printf("%d\n",end);
-while(start<end)
+    int pivot = a[lb], temp, start = lb, end = ub;
+    while (start < end)
     {
-        while(a[start]<=pivot)
+        while (a[start] <= pivot)
         {
             start++;
         }
-        while(pivot<a[end])
+        while (pivot < a[end])
         {
             end--;
         }
-       // printf("%d  %d\n",a[start],a[end]);
-        if(start<end)
+        if (start < end)
         {
-            temp=a[start];
-            a[start]=a[end];
-            a[end]=temp;
+            // swap a[start], a[end]
+            temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
         }
     }
-
-    temp=a[lb];
-    a[lb]=a[end];
-    a[end]=temp;
-  //  printf("%d\n",end);
+    // swap a[lb], a[end]
+    temp = a[lb];
+    a[lb] = a[end];
+    a[end] = temp;
     return end;
 }
 
-void Quicksort(int *a,int lb,int ub)
+void Quicksort(int *a, int lb, int ub)
 {
     int loc;
-    if(lb<ub)
+    if (lb < ub)
     {
-        loc=Position(a,lb,ub);
-        //printf("%d\n",loc);
-        Quicksort(a,lb,loc-1);
-        Quicksort(a,loc+1,ub);
+        loc = Position(a, lb, ub);
+        Quicksort(a, lb, loc - 1);
+        Quicksort(a, loc + 1, ub);
     }
 }
 int main()
 {
-    int a[]={7,6,10,88,9,1,15,7},lb=0,ub=sizeof(a)/sizeof(a[0])-1;
-    Quicksort(a,lb,ub);
+    int a[] = {7, 6, 10, 88, 9, 1, 15, 7}, lb = 0, ub = sizeof(a) / sizeof(a[0]) - 1;
+    Quicksort(a, lb, ub);
 
-for(int i=0;i<8;i++)
+    for (int i = 0; i < 8; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
     return 0;
 }
