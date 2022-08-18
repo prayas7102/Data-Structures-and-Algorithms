839f8c33bb3d5af52bb5e295a933a846b7eb9854
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-08-18 20:02:42 +0530

    reverse k elements in queue

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <deque>
 #include <queue>
+#include <stack>
 #include <vector>
 using namespace std;
 
@@ -43,26 +44,30 @@ void findFirstNegative(vector<int> v, int k)
 	}
 	else
 	{
-		dq.push_back(0);
+		ans.push_back(0);
 	}
 
 	for (int i = k; i < v.size(); ++i)
 	{
-		if (!dq.empty() && i - dq.front() >= k)
+		// deleting element to check k elements in dq
+		// diff b/w i and dq.front() <= k
+		if (!dq.empty() && i >= k+dq.front())
 		{
 			dq.pop_front();
 		}
+		// adding element
 		if (v[i] < 0)
 		{
 			dq.push_back(i);
 		}
+		// ans array updated
 		if (dq.size())
 		{
 			ans.push_back(v[dq.front()]);
 		}
 		else
 		{
-			dq.push_back(0);
+			ans.push_back(0);
 		}
 	}
 	for (auto a : ans)
@@ -70,6 +75,31 @@ void findFirstNegative(vector<int> v, int k)
 		cout << a << " ";
 	}
 }
+
+void reverseKelementsQueue(queue<int> &q, int k)
+{
+	stack<int>s;
+	int n=q.size();
+	for (int i = 0; i < k; ++i)
+	{
+		int ans = q.front();
+		s.push(ans);
+		q.pop();
+	}
+	for (int i = 0; i < k; ++i)
+	{
+		int ans = s.top();
+		q.push(ans);
+		s.pop();
+	}
+	for (int i = 0; i < n-k; ++i)
+	{
+		int ans = q.front();
+		q.push(ans);
+		q.pop();
+	}
+}
+
 int main()
 {
 	// reverse a queue
@@ -84,9 +114,15 @@ int main()
 
 	// first negative elements for k elements
 	cout << "first negative elements for k elements" << endl;
-	vector<int> v{-1, 2, 3, -4, 5, -6};
+	vector<int> v{1, -2, 3, -4, 5, 6};
 	int k = 2;
 	findFirstNegative(v, k);
+
+	// reverse first k elements of queue
+	cout << "\n\nreverse first k elements of queue = ";
+	printQueue(q1);
+	reverseKelementsQueue(q1,k);
+	printQueue(q1);
 
 	return 0;
 }
