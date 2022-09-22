47163270f3e9d6e1ea5c7aa1574790b8d33f04c3
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-09-22 21:07:08 +0530

    recursive isPalindrome, reverseString

--- Recursion/Recursion_Standard_Question/recursion.cpp
+++ Recursion/Recursion_Standard_Question/recursion.cpp
@@ -31,12 +31,38 @@ int binarySearch(vector<int> a, int k)
 	return -1;
 }
 
+string reverseString(string s, int low, int high)
+{
+	if (low >= high)
+		return s;
+	swap(s[low], s[high]);
+	return reverseString(s, low + 1, high - 1);
+}
+
+bool isPalindrome(string s, int low, int high)
+{
+	if (low >= high)
+		return 1;
+	if (s[low] == s[high])
+		return true;
+	else
+		return false;
+}
+
 int main()
 {
 	vector<int> a{0, 3, 5, 6, 12, 19};
 	int num = 12;
 	cout << "Position of " << num << " using binarySearch " << binarySearch(a, num) << endl;
-	cout << "Position of " << num << " using binarySearch recursive " << binarySearchRecursive(a, num, 0, a.size()) << endl;
+	cout << "Position of " << num << " using binarySearch recursive " << binarySearchRecursive(a, num, 0, a.size()) << endl
+		 << endl;
 
+	string str = "shalini_kutiya_hai";
+	str = reverseString(str, 0, str.size() - 1);
+	cout << str << " = string reversed to = " << reverseString(str, 0, str.size() - 1) << endl;
+
+	str = "10";
+	string ans = isPalindrome(str, 0, str.size() - 1) ? "a Palindrome" : "not a palindronme";
+	cout << str << " is " << ans << "\n\n";
 	return 0;
 }
