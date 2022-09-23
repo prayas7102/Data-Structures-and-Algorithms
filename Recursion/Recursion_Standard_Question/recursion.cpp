54094aa65671e9376240cc2252e77d497a758f05
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-09-23 18:12:27 +0530

    array subset, string permutation

--- Recursion/Recursion_Standard_Question/recursion.cpp
+++ Recursion/Recursion_Standard_Question/recursion.cpp
@@ -49,9 +49,55 @@ bool isPalindrome(string s, int low, int high)
 		return false;
 }
 
+void arraySubset(vector<vector<int>> &ans, vector<int> output, int index, vector<int> num)
+{
+	if (index >= num.size())
+	{
+		ans.push_back(output);
+		return;
+	}
+
+	// exclusion of elements
+	arraySubset(ans, output, index + 1, num);
+
+	output.push_back(num[index]);
+
+	// inclusion of elements
+	arraySubset(ans, output, index + 1, num);
+}
+
+void subsetString(vector<string> &ans, string output, int index, string str)
+{
+	if (index >= str.size())
+	{
+		ans.push_back(output);
+		return;
+	}
+	// exclusion of elements
+	subsetString(ans, output, index + 1, str);
+	output.push_back(str[index]);
+	// inclusion of elements
+	subsetString(ans, output, index + 1, str);
+}
+
+void stringPermutation(vector<string> &ans, int index, string str)
+{
+	if (index >= str.size())
+	{
+		ans.push_back(str);
+		return;
+	}
+	for (int i = index; i < str.size(); i++)
+	{
+		swap(str[i], str[index]);
+		stringPermutation(ans, index + 1, str);
+		swap(str[i], str[index]);
+	}
+}
+
 int main()
 {
-	vector<int> a{0, 3, 5, 6, 12, 19};
+	vector<int> a{0, 3, 6, 12};
 	int num = 12;
 	cout << "Position of " << num << " using binarySearch " << binarySearch(a, num) << endl;
 	cout << "Position of " << num << " using binarySearch recursive " << binarySearchRecursive(a, num, 0, a.size()) << endl
@@ -59,10 +105,50 @@ int main()
 
 	string str = "shalini_kutiya_hai";
 	str = reverseString(str, 0, str.size() - 1);
-	cout << str << " = string reversed to = " << reverseString(str, 0, str.size() - 1) << endl;
+	cout << str << " = string reversed to = " << reverseString(str, 0, str.size() - 1) << endl
+		 << endl;
 
 	str = "10";
 	string ans = isPalindrome(str, 0, str.size() - 1) ? "a Palindrome" : "not a palindronme";
 	cout << str << " is " << ans << "\n\n";
+
+	vector<vector<int>> answer;
+	vector<int> output;
+	cout << "Following is the recursive combinations" << endl;
+	arraySubset(answer, output, 0, a);
+	for (auto i : answer)
+	{
+		for (auto j : i)
+			cout << j << " ";
+		cout << endl;
+	}
+
+	cout << endl;
+
+	string test = "ram";
+	string output2;
+	vector<string> answer2;
+	subsetString(answer2, output2, 0, test);
+	cout << "Following is the recursive combinations" << endl;
+	for (auto i : answer2)
+	{
+		for (auto &j : i)
+			cout << j << " ";
+		cout << endl;
+	}
+
+	cout << endl;
+
+	vector<string> answer3;
+	stringPermutation(answer3, 0, test);
+	cout << "Following is the recursive permutation" << endl;
+	for (auto i : answer3)
+	{
+		for (auto &j : i)
+			cout << j << " ";
+		cout << endl;
+	}
+	cout << endl;
+
 	return 0;
 }
