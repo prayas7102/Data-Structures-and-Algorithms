77373b9dcb05f32137c31418fb98d3e6b77b3e1f
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2023-01-12 15:27:31 +0530

    test

--- Dynamic Programming/dp.cpp
+++ Dynamic Programming/dp.cpp
@@ -50,6 +50,8 @@ int frogJump(int ind, vector<int> &v, vector<int> dp)
 
 int frogJumpTabulation(vector<int> &v, vector<int> dp)
 {
+    // frog can jump either one or 2 steps forward
+    // find the min cost of jumping 
 
     dp[0] = 0;
     int n = v.size();
@@ -68,8 +70,38 @@ int frogJumpTabulation(vector<int> &v, vector<int> dp)
     return dp[n - 1];
 }
 
+int houseRob(vector<int>& nums, int n, vector<int>& dp){
+    // no adjacent element of array can be selected
+    // rertrun max sum of non adj elements
+
+        if(n==0) return nums[n];
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int left=nums[n]+houseRob(nums,n-2,dp);
+        int right=houseRob(nums,n-1,dp);
+        return dp[n]=max(left,right);
+    }
+
+int houseRobTabulation(vector<int>& nums, int n, vector<int>& dp){
+    // no adjacent element of array can be selected
+    // rertrun max sum of non adj elements
+    
+}
+
 int main()
 {
+    int k=3;
+    vector<int> v1{1,4,7,5,3};
+    int sum=0;
+    for(auto a:v1){
+        int diff=a-k;
+        sum+=((diff)<0)?diff*-1:diff;
+    }
+    cout<<sum;
+    
+
+    
+
     int n = 10;
     vector<int> dp(n + 1, -1);
     cout << n << "th fibbonacciNo using memoization " << fibbonacciNo(n, dp) << "\n\n";
@@ -81,6 +113,6 @@ int main()
     vector<int> dp1(m + 1, -1);
     cout << "minimum energy for frog jump (memoization): " << frogJump(m - 1, v, dp1) << "\n\n";
     cout << "minimum energy for frog jump (tabulation): " << frogJumpTabulation(v, dp1) << "\n\n";
-
+    // cout<<
     return 0;
 }
